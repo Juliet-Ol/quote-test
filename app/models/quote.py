99eedc45ee05259c762e1d470d81f3d30a51e434
Juliet <agutu@gmail.com>
2022-03-13 22:51:08 +0300

    Class: create a quote class that defines our quote

---
+++ app/models/quote.py
@@ -0,0 +1,10 @@
+class Quote:
+    '''
+    Class that defines quote 
+    '''
+
+    def __init__(self, author, id, quote, permalink):
+        self.author = author 
+        self.id = id
+        self.quote = quote
+        self.permalink = permalink
